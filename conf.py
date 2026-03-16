737f950312885813ed253c967a8047575b1871b1
shipkovenska <162316400+shipkovenska@users.noreply.github.com>
2026-03-16 16:50:39 +0200

    Добавяне на нова тема Касови документи

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Dreem User's Manual"
-copyright = "2025 by Unicontsoft"
+copyright = "2026 by Unicontsoft"
 author = "Unicontsoft"
 
 # -- General configuration ---------------------------------------------------
